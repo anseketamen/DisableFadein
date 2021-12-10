cb62063744cdc06a59e6c1f16959cbc91b33fc90
Anseketamen <17738954+Anseketamen@users.noreply.github.com>
2021-12-10 22:08:22 +0900

    プロジェクト ファイルを追加します。

---
+++ FormMain.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace DisableFadein
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.StartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.PauseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StartToolStripMenuItem,
+            this.PauseToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.ExitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(187, 124);
+            // 
+            // StartToolStripMenuItem
+            // 
+            this.StartToolStripMenuItem.Name = "StartToolStripMenuItem";
+            this.StartToolStripMenuItem.Size = new System.Drawing.Size(186, 38);
+            this.StartToolStripMenuItem.Text = "再開";
+            this.StartToolStripMenuItem.Click += new System.EventHandler(this.StartMenu_Clicked);
+            // 
+            // PauseToolStripMenuItem
+            // 
+            this.PauseToolStripMenuItem.Name = "PauseToolStripMenuItem";
+            this.PauseToolStripMenuItem.Size = new System.Drawing.Size(186, 38);
+            this.PauseToolStripMenuItem.Text = "一時停止";
+            this.PauseToolStripMenuItem.Click += new System.EventHandler(this.PauseMenu_Clicked);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(183, 6);
+            // 
+            // ExitToolStripMenuItem
+            // 
+            this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
+            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(186, 38);
+            this.ExitToolStripMenuItem.Text = "終了";
+            this.ExitToolStripMenuItem.Click += new System.EventHandler(this.ExitMenu_Clicked);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem StartToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem PauseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem ExitToolStripMenuItem;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Media;
+
+namespace DisableFadein
+{
+    public partial class FormMain : Form
+    {
+        /// <summary>
+        /// 無音の音源
+        /// </summary>
+        private Stream silentAudioStream { get; } = Properties.Resources.silent;
+
+        /// <summary>
+        /// 音源流すやつ
+        /// </summary>
+        private SoundPlayer player;
+
+        public FormMain()
+        {
+            InitializeComponent();
+            player = new SoundPlayer(silentAudioStream);
+
+            this.Load += (_, __) => StartPlaying();
+        }
+
+        private void StartMenu_Clicked(object sender, EventArgs e) => StartPlaying();
+        private void ExitMenu_Clicked(object sender, EventArgs e) => ExitApplication();
+        private void PauseMenu_Clicked(object sender, EventArgs e) => PausePlaying();
+
+        /// <summary>
+        /// 無音ファイル再生状態にします
+        /// </summary>
+        private void StartPlaying()
+        {
+            if (player == null)
+            {
+                ExitApplication();
+            }
+            player.Stop();
+            player.PlayLooping();
+
+            StartToolStripMenuItem.Enabled = false;
+            PauseToolStripMenuItem.Enabled = true;
+            notifyIcon1.Text = "DisableFadein" + "(動作中)";
+        }
+
+        /// <summary>
+        /// 無音ファイル一時停止状態にします
+        /// </summary>
+        private void PausePlaying()
+        {
+            if (player == null)
+            {
+                ExitApplication();
+            }
+            player.Stop();
+            StartToolStripMenuItem.Enabled = true;
+            PauseToolStripMenuItem.Enabled = false;
+            notifyIcon1.Text = "DisableFadein" + "(停止中)";
+        }
+
+        /// <summary>
+        /// アプリを終了します
+        /// </summary>
+        private void ExitApplication() => Application.Exit();
+
+        /// <summary>
+        /// FormMain.FormClosingイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //アイコン消してからExitしないとタスクバーにゴミが残る
+            notifyIcon1.Visible = false;
+            Invalidate();
+
+            //音源止める
+            player?.Stop();
+            player?.Dispose();
+            player = null;
+        }
+    }
+}
